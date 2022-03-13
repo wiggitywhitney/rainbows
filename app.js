8fc682868c01309b53619e10a424c9ffe2820c13
Steven Pousty <steve.pousty@gmail.com>
2022-03-13 11:43:08 -0700

    You had a typo

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Suck it, Steve!');
+  res.send('Suck it, Whitney!');
 });
 
 app.listen(3000, function () {
