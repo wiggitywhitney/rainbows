fac0bd2b47c7107e3f3b6a0128fce069e3b474be
wiggitywhitney <47868428+wiggitywhitney@users.noreply.github.com>
2022-06-09 16:37:56 -0500

    Now it is about rainbows

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Suck it, Steve!');
+  res.send('pretend this is some rainbows');
 });
 
 app.listen(3000, function () {
-  console.log('Suck it Steve listening on port 3000!');
+  console.log('rainbows app listening on port 3000!');
 });
