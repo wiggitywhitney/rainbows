09ede0112022c357c1114dcaf9c5b7edf478727f
wiggitywhitney <47868428+wiggitywhitney@users.noreply.github.com>
2022-06-09 16:36:57 -0500

    Suck it Steve

--- app.js
+++ app.js
@@ -6,5 +6,5 @@ app.get('/', function (req, res) {
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Suck it Steve listening on port 3000!');
 });
