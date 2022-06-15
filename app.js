2797b47ab00aa4d7f6e03d9babc26769d3535698
wiggitywhitney <47868428+wiggitywhitney@users.noreply.github.com>
2022-06-15 15:20:54 -0500

    Serves rainbows image

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static('public'));
+
 app.get('/', function (req, res) {
-  res.send(images/rainbowsrainbowsrainbows.png);
+  res.send("Rainbows! Rainbows! RAINBOWS!");
 });
 
 app.listen(3000, function () {
