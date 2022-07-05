4225f800fb81278350da137858a9dec79fce080d
wiggitywhitney <47868428+wiggitywhitney@users.noreply.github.com>
2022-07-05 14:06:29 -0500

    Change to serve on port 8080

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ app.get('/', function (req, res) {
   res.send("Spiders! Spiders! SPIDERS!");
 });
 
-app.listen(3000, function () {
-  console.log('rainbows app listening on port 3000!');
+app.listen(8080, function () {
+  console.log('rainbows app listening on port 8080!');
 });
