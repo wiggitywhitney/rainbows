208ed563f4726af2a9806707bd90e379fd248299
wiggitywhitney <47868428+wiggitywhitney@users.noreply.github.com>
2022-08-20 20:11:47 -0500

    Send exit code 0

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
+const http = require('http');
 var express = require('express');
 var app = express();
+const { createTerminus, HealthCheckError } = require('@godaddy/terminus');
 
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-  res.send("Rainbows! Rainbows! RAINBOWS!");
+  res.send("Spiders! Rainbows! SPIDERS AND RAINBOWS AND SMILES!");
 });
 
-app.listen(8080, function () {
+const server = http.createServer(app);
+
+server.listen(8080, function () {
   console.log('rainbows app listening on port 8080!');
 });
+
+createTerminus(server, {
+  useExit0: true
+})
